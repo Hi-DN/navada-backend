081bcd772d5a89fb89e71ba7932353cc715ca84f
minji1110 <kalswl991110@gmail.com>
2022-04-13 12:42:07 +0900

    navada first commit

---
+++ src/main/java/hidn/navada/NavadaApplication.java
@@ -0,0 +1,13 @@
+package hidn.navada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hidn/navada/NavadaApplicationTests.java
@@ -0,0 +1,13 @@
+package hidn.navada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
