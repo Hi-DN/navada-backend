eef32cd9510efa1639f4ff12cd6df761b35813ef
hanslelee <hssarah13@gmail.com>
2022-05-28 02:11:28 +0900

    Timezone, import.sql 수정

--- src/main/java/hidn/navada/NavadaApplication.java
+++ src/main/java/hidn/navada/NavadaApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class NavadaApplication {
@@ -12,4 +15,8 @@ public class NavadaApplication {
 		SpringApplication.run(NavadaApplication.class, args);
 	}
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
