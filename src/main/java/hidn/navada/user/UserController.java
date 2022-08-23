15dacc2488ba46ed433f96a462488e2d752b0724
hanslelee <hssarah13@gmail.com>
2022-08-23 21:33:37 +0900

    회원 단건 조회

--- src/main/java/hidn/navada/user/UserController.java
+++ src/main/java/hidn/navada/user/UserController.java
@@ -1,6 +1,10 @@
 package hidn.navada.user;
 
+import hidn.navada.comm.response.ResponseService;
+import hidn.navada.comm.response.SingleResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping(value = "/v1")
 public class UserController {
+    private final UserService userService;
+    private final ResponseService responseService;
 
+    // 회원 단건 조회
+    @GetMapping(value = "/user/{userId}")
+    public SingleResponse<User> getUser(@PathVariable long userId){
+        return responseService.getSingleResponse(userService.getOneUser(userId));
+    }
 }
--- src/main/java/hidn/navada/user/UserService.java
+++ src/main/java/hidn/navada/user/UserService.java
@@ -1,9 +1,16 @@
 package hidn.navada.user;
 
+import hidn.navada.comm.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserJpaRepo userJpaRepo;
+
+    // 회원 단건 조회
+    public User getOneUser(long userId){
+        return userJpaRepo.findById(userId).orElseThrow(UserNotFoundException::new);
+    }
 }
