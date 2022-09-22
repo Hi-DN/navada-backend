7bbccee473ca4936b27a3c156d5d45f16579c2dc
minji1110 <kalswl991110@gmail.com>
2022-09-23 01:08:18 +0900

    proxy 반환 해결 (임시) : fetch join

--- src/main/java/hidn/navada/exchange/ExchangeJpaRepo.java
+++ src/main/java/hidn/navada/exchange/ExchangeJpaRepo.java
@@ -8,12 +8,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ExchangeJpaRepo extends JpaRepository<Exchange, Long> {
-    @Query(value = "select e from Exchange e where (e.acceptor=:user and e.acceptorHistoryDeleteYn=false) or (e.requester=:user and e.requesterHistoryDeleteYn=false)")
+
+    @Query(value = "select e from Exchange e join fetch e.requesterProduct join fetch e.acceptorProduct " +
+            "where (e.acceptor=:user and e.acceptorHistoryDeleteYn=false) or (e.requester=:user and e.requesterHistoryDeleteYn=false)",
+    countQuery = "select count(e) from Exchange e inner join e.requesterProduct inner join e.acceptorProduct " +
+            "where (e.acceptor=:user and e.acceptorHistoryDeleteYn=false) or (e.requester=:user and e.requesterHistoryDeleteYn=false)")
     Page<Exchange> findExchangePagesByUser(@Param("user") User user, Pageable pageable);
 
-    @Query(value = "select e from Exchange e where (e.requester=:user and e.requesterHistoryDeleteYn=false)")
+    @Query(value = "select e from Exchange e join fetch e.requesterProduct join fetch e.acceptorProduct " +
+            "where (e.requester=:user and e.requesterHistoryDeleteYn=false)",
+    countQuery = "select count(e) from Exchange e inner join e.requesterProduct inner join e.acceptorProduct " +
+            "where (e.requester=:user and e.requesterHistoryDeleteYn=false)")
     Page<Exchange> findExchangePagesByRequester(@Param("user") User user, Pageable pageable);
 
-    @Query(value = "select e from Exchange e where (e.acceptor=:user and e.acceptorHistoryDeleteYn=false)")
+    @Query(value = "select e from Exchange e join fetch e.acceptorProduct join fetch e.requesterProduct " +
+            "where (e.acceptor=:user and e.acceptorHistoryDeleteYn=false)",
+    countQuery ="select count(e) from Exchange e inner join e.acceptorProduct inner join e.requesterProduct " +
+            "where (e.acceptor=:user and e.acceptorHistoryDeleteYn=false)" )
     Page<Exchange> findExchangePagesByAcceptor(@Param("user") User user, Pageable pageable);
 }
