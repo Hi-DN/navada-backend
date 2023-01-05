a07d2ccf17cd2472d648db6158c425faf1a9cf92
minji1110 <kalswl991110@gmail.com>
2023-01-05 18:35:24 +0900

    교환 신청 일자 추가

--- src/main/java/hidn/navada/exchange/request/RequestDto.java
+++ src/main/java/hidn/navada/exchange/request/RequestDto.java
@@ -3,6 +3,8 @@ package hidn.navada.exchange.request;
 import hidn.navada.product.Product;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,6 +13,8 @@ public class RequestDto {
 
     private char requestStatusCd;       //신청상태(0 : 대기 , 1 : 수락, 2 : 거절)
 
+    private LocalDateTime requestCreatedDt;     // 교환 신청 일자
+
     private Product requesterProduct;
 
     private Product acceptorProduct;
@@ -18,6 +22,7 @@ public class RequestDto {
     public RequestDto(Request request){
         requestId=request.getRequestId();
         requestStatusCd=request.getRequestStatusCd();
+        requestCreatedDt = request.getCreatedDate();
         requesterProduct = request.getRequesterProduct();
         acceptorProduct = request.getAcceptorProduct();
     }
